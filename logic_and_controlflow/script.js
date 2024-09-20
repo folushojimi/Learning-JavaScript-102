56f23a92630d3ffcd70f17ab5af9f7b96ec974a2
folushojimi <iamrblogger@gmail.com>
2024-09-20 23:57:18 +0100

    finalcode

--- logic_and_controlflow/script.js
+++ logic_and_controlflow/script.js
@@ -1,4 +1,13 @@
 //logic and control flow in javascript 
 
 //The "while" loop 
-while()
+let i = 0
+
+while(i < 10){
+console.log(i)
+i++;
+}
+// This simple while loop is written to loop around the
+//number of 10, 10 times. 
+
+
