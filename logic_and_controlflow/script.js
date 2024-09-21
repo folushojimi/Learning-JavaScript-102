5091415b2bbc28036e55334ad2669f4aa02cd4a5
folushojimi <iamrblogger@gmail.com>
2024-09-21 10:13:05 +0100

    code updated

--- logic_and_controlflow/script.js
+++ logic_and_controlflow/script.js
@@ -1,4 +1,10 @@
 //logic and control flow in javascript 
+//The 3 terminology of everyloops in javascript 
+//1.initailise
+//2.condition
+//3.final expression
+
+
 
 //The "while" loop 
 let i = 0
@@ -11,3 +17,15 @@ i++;
 //number of 10, 10 times. 
 
 
+//for loop
+for (let i = 0; i < 10; i++){
+    console.log(i)
+}
+
+
+//for...in loop
+
+//for... of loop 
+
+//do while loop 
+ 
